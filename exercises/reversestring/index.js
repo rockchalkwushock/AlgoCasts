5cf10f0b1db6edef3aee349ddead4bd1a1b863c1
Cody Brunner <rockchalkwushock@icloud.com>
2017-12-04 08:06:30 -0600

    reversedstring done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  if (typeof str !== 'string') return 1
+  // REVIEW Solution #1
+  // return str.split('').reverse().join('')
+
+  // REVIEW Solution #2
+  // let reversed = ''
+  // for (let char of str) {
+  //   reversed = char + reversed
+  // }
+  // return reversed
+
+  // REVIEW Solution #3
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;

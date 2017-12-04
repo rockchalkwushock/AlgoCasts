24622dd85af1f6c47d9ce192481f7732d51d18b3
Cody Brunner <rockchalkwushock@icloud.com>
2017-12-04 08:41:39 -0600

    reverseint done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // REVIEW My Solution
+  if (typeof n !== 'number') return 1
+  // if (n > 0) {
+  //   return parseInt(n.toString().split('').reverse().join(''))
+  // } else if (n === 0) {
+  //   return n
+  // } else {
+  //   return parseInt((n * -1).toString().split('').reverse().join('')) * -1
+  // }
+  // REVIEW Condensed Grider Solution
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
+}
 
 module.exports = reverseInt;

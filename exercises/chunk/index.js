574a16242928536264b0b593c30ac0debe33a561
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-06 11:15:40 -0500

    chunk done.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // const chunked = []
 
-module.exports = chunk;
+  // for (const el of array) {
+  //   const last = chunked[chunked.length - 1]
+
+  //   if (!last || last.length === size) {
+  //     chunked.push([el])
+  //   } else {
+  //     last.push(el)
+  //   }
+  // }
+
+  // return chunked
+
+  const chunked = []
+  let index = 0
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
+  return chunked
+}
+
+module.exports = chunk

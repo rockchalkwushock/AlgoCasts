a910423296c1de6358f3733ad257d4a6d4dc8f0b
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-06 10:27:27 -0500

    palindrome done.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,22 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  // const reversed = str
+  //   .split('')
+  //   .reverse()
+  //   .join('')
+  // return str === reversed
 
-module.exports = palindrome;
+  // let reversed = ''
+  // for (const char of str) {
+  //   reversed = char + reversed
+  // }
+  // return str === reversed
+
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  })
+}
+
+module.exports = palindrome

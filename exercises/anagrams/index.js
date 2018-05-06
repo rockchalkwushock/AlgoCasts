d560fb4c7e1becaa1f69c24d2f62d55c51917eb3
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-06 11:32:33 -0500

    anagrams done.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // const aCharMap = buildCharMap(stringA)
+  // const bCharMap = buildCharMap(stringB)
+  // if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+  //   return false
+  // }
+  // for (const char in aCharMap) {
+  //   if (aCharMap[char] !== bCharMap[char]) {
+  //     return false
+  //   }
+  // }
+  // return true
 
-module.exports = anagrams;
+  return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g)
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('')
+}
+
+function buildCharMap(str) {
+  const charMap = {}
+
+  for (const char of str.replace(/[^\w]/g).toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
+
+module.exports = anagrams

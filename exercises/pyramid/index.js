939e91613a3c586ad748eb6af773fdf6b3c6b730
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-07 06:42:04 -0500

    pyramid done.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  const midpoint = Math.floor((2 * n - 1) / 2)
+  // Base Case
+  if (n === row) {
+    return
+  }
+  // Yield the row.
+  if (level.length === 2 * n - 1) {
+    console.log(level)
+    return pyramid(n, row + 1)
+  }
+  // Build the level dependent on the location.
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    level += '#'
+  } else {
+    level += ' '
+  }
+  pyramid(n, row, level)
+}
 
-module.exports = pyramid;
+// Solution #1
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2)
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#'
+//       } else {
+//         level += ' '
+//       }
+//     }
+//     console.log(level)
+//   }
+// }
+
+module.exports = pyramid

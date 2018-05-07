8fdf8d5eb0cef18d03b2a80aee4a0f01672e4e2c
Cody Brunner <rockchalkwushock@icloud.com>
2018-05-07 08:23:39 -0500

    fib done.

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,44 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {}
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+    const result = fn.apply(this, args)
+    cache[args] = result
+    return result
+  }
+}
 
-module.exports = fib;
+function fib(n) {
+  if (n < 2) {
+    return n
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
+fib = memoize(fib)
+
+// Solution #2 (recursive)
+// function fib(n) {
+//   if (n < 2) {
+//     return n
+//   }
+//   return fib(n - 1) + fib(n - 2)
+// }
+
+// Solution #1 (iterative)
+// function fib(n) {
+//   const result = [0, 1]
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1]
+//     const b = result[i - 2]
+//     result.push(a + b)
+//   }
+//   return result[n]
+// }
+
+module.exports = fib
